7903e378971f44257ab9d8c2d8263a090540f2c6
Sude Özübek <130099053+Sudeozubek@users.noreply.github.com>
2025-02-12 23:05:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from models.models import Base
+from database.database import engine
+from fastapi.middleware.cors import CORSMiddleware
+from routers.note import router as notes_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(notes_router)
+
+@app.get("/")
+async def home():
+    return {"message": "AI-Powered Notes App is running"}
